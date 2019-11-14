df673b766799caacc5721d3ba440544febbd8a36
Nikodem Bienia <nikodem.bienia@gmail.com>
2019-11-14 22:41:04 +0100

    shareuser create in setup

--- setup.sh
+++ setup.sh
@@ -6,6 +6,15 @@
 ## install samba
 ##sudo apt-get install samba samba-common-bin
 
+## check is user: shareuser exists
+if id "shareuser" >/dev/null 2>&1;
+then 
+ echo "user: shareuser exists"
+else
+ echo "user: shareuser does not exist"
+ sudo adduser --system shareuser
+fi
+
 ## check if 
 if [ ! -d "/home/pi/shared" ] 
 then
@@ -14,6 +23,9 @@ then
 else 
  echo "shared folder existing"
 fi
+
+## change ownership of /home/pi/shared/ folder to shareuser
+sudo chown -R shareuser /home/pi/shared/
 
 ## check for user and add to smb.conf file if necessary
 if grep -Fxq "[pimylifeupshare]" /etc/samba/smb.conf
